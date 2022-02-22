7fd45ae9d8222e282119847ab2aef9ed6f4e88e5
NickyB <nickbaranosky@gmail.com>
2022-02-22 17:34:09 -0500

    Add files via upload
    1.0 Commit

--- server.js
+++ server.js
@@ -3,10 +3,14 @@ const path = require("path");
 
 const app = express();
 
+console.log("Parameters established...")
+
 app.use("/static", express.static(path.resolve(__dirname, "frontend", "static")));
+console.log("Path resolved...")
 
 app.get("/*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "frontend", "index.html"));
 });
+console.log("Files initialized...")
 
-app.listen(process.env.PORT || 5080, () => console.log("Server running..."));
+app.listen(process.env.PORT || 5080, () => console.log("System is running!"));
